ee12f2272f2fd80f7c4c521b0f344cae7968d21c
Pradeep-SL <121848914+Pradeep-SL@users.noreply.github.com>
2023-01-05 22:56:41 +0530

    Update Solution - Copy.java

--- Solution - Copy.java
+++ Solution - Copy.java
@@ -2,7 +2,7 @@ class Solution{
     public static void main(String[] args) {
         System.out.println("Testing github");
 
-		//adding a comment to test out git bash
+		//adding a comment to test out git bugs
 
     }
 
